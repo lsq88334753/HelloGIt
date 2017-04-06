c5b65e5b0553587e5ea7c6f8060f334fe35d1777
lsq <453827835@qq.com>
2017-04-06 12:20:03 +0800

    partener 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("new project"); // init
         System.out.println("partener");
+        System.out.println("you gai le");
     }
 
 }
