8a95073b24713dd5d30c595eaa784e993e3765d3
lsq <453827835@qq.com>
2017-04-06 12:50:58 +0800

    Merge branch 'master' of github.com:lsq88334753/HelloGit into branch1
    # Conflicts:
    #	src/main/java/Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,9 +4,11 @@
 public class Test {
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
+        System.out.println("new project"); // init
+        System.out.println("partener");
+        System.out.println("you gai le");
+        System.out.println("bug1 fix");
+        System.out.println("you gai le");
     }
 
 }
